03f32964cd240c20477889781013a3288f095966
full-mind <admin@fullmind.cl>
2023-01-05 23:53:26 -0500

    Merge branch 'master' of https://github.com/full-mind/frp-landing-home

--- src/utils/ActiveLink.js
+++ src/utils/ActiveLink.js
@@ -1,12 +1,13 @@
 import { withRouter } from 'next/router';
 import Link from 'next/link';
 import React, { Children } from 'react';
+import { isUndefined, get } from "lodash";
 
 const ActiveLink = ({ router, children, ...props }) => {
     const child = Children.only(children);
     console.log("router",router)
     let className = child.props.className || '';
-    if (router!=undefined&& router.pathname === props.href && props.activeClassName) {
+    if (!isUndefined(router) && get(router,"pathname","") === props.href && props.activeClassName) {
         className = `${className} ${props.activeClassName}`.trim();
     }
 
